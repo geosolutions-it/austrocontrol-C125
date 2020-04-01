90a858c924330c0c23c1daa79834dca5325a4c39
mv88 <matteo.velludini@gmail.com>
2020-04-01 18:27:17 +0200

    fixing build
    (cherry picked from commit 7e4a6a1cb0cf4218c5afc45eb84e4803e2baefc3)

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
-const path = require("path");
-
+const path = require('path');
 const themeEntries = require('./MapStore2/build/themes.js').themeEntries;
 const extractThemesPlugin = require('./MapStore2/build/themes.js').extractThemesPlugin;
 const assign = require('object-assign');
 
-module.exports = assign({},require('./MapStore2/build/buildConfig')(
+module.exports = assign({}, require('./MapStore2/build/buildConfig')(
     {
-        'austrocontrol-ms2': path.join(__dirname, "js", "app"),
-
+        'austrocontrol-ms2': path.join(__dirname, "js", "app")
     },
     themeEntries,
     {
@@ -19,25 +17,27 @@ module.exports = assign({},require('./MapStore2/build/buildConfig')(
     extractThemesPlugin,
     false,
     "/dist/",
-    '.austrocontrol-ms2'
-), {
-    devServer: {
-        proxy: {
-            '/rest/geostore': {
-                target: "https://austrocontrol.geo-solutions.it/austrocontrol-ms2",
-                secure: false
-            },
-            '/pdf': {
-                target: "https://austrocontrol.geo-solutions.it/austrocontrol-ms2",
-                secure: false
-            },
-            '/proxy': {
-                target: "https://austrocontrol.geo-solutions.it/austrocontrol-ms2"
-            },
-            '/docs': {
-                target: "http://localhost:8081",
-                pathRewrite: { '/docs': '/mapstore/docs' }
-            }
+    '.austrocontrol-ms2',
+    null,
+    {
+        '@mapstore': path.resolve(__dirname, 'MapStore2/web/client'),
+        '@js': path.resolve(__dirname, 'js')
+    },
+    {
+        '/rest/geostore': {
+            target: "https://austrocontrol.geo-solutions.it/austrocontrol-ms2",
+            secure: false
+        },
+        '/pdf': {
+            target: "https://austrocontrol.geo-solutions.it/austrocontrol-ms2",
+            secure: false
+        },
+        '/proxy': {
+            target: "https://austrocontrol.geo-solutions.it/austrocontrol-ms2"
+        },
+        '/docs': {
+            target: "http://localhost:8081",
+            pathRewrite: { '/docs': '/mapstore/docs' }
         }
     }
-});
+));
